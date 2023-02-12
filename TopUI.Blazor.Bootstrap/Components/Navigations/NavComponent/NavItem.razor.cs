020d581eb2809f0fa9af420783c0f245411368d8
Vahid Rassouli <vrassouli@live.com>
2023-02-12 18:31:42 +0330

    NavItem Icons added

--- TopUI.Blazor.Bootstrap/Components/Navigations/NavComponent/NavItem.razor.cs
+++ TopUI.Blazor.Bootstrap/Components/Navigations/NavComponent/NavItem.razor.cs
@@ -12,6 +12,7 @@ public sealed partial class NavItem
     [CascadingParameter] public Nav Nav { get; set; } = default!;
     [CascadingParameter] public NavItem? ParentItem { get; set; }
     [Parameter] public string Title { get; set; } = "Item";
+    [Parameter] public string? Icon { get; set; }
     [Parameter] public string Url { get; set; } = "#";
     [Parameter] public string? LinkTarget { get; set; }
     [Parameter] public RenderFragment? ChildContent { get; set; }
