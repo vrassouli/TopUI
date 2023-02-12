e5544b61b2c56584aca6caf253be801acdb35117
Vahid Rassouli <vrassouli@live.com>
2023-02-12 20:29:26 +0330

    ListGroup EmptyMessage added

--- TopUI.Blazor.Bootstrap/Components/Lists/ListGroupComponent/DataListGroup.cs
+++ TopUI.Blazor.Bootstrap/Components/Lists/ListGroupComponent/DataListGroup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using TopUI.Blazor.Bootstrap.Components.Utilities;
@@ -12,6 +14,8 @@ namespace TopUI.Blazor.Bootstrap.Components;
 
 public sealed class DataListGroup<TItem> : ListGroup, IDataBoundComponent<TItem>, IDataSelectionContainer<TItem>
 {
+    private RenderFragment? _emptyTemplate;
+
     [Parameter] public IList<TItem>? Items { get; set; }
     [Parameter] public ItemsProviderDelegate<TItem>? ItemsProvider { get; set; }
     [Parameter] public TItem? SelectedItem { get; set; } = default!;
@@ -23,6 +27,10 @@ public sealed class DataListGroup<TItem> : ListGroup, IDataBoundComponent<TItem>
     [Parameter] public Func<TItem, string?>? ItemUrl { get; set; }
     [Parameter] public Func<TItem, ListGroupItemMode>? ItemMode { get; set; }
     [Parameter] public RenderFragment<TItem>? ItemTemplate { get; set; }
+    [Parameter] public RenderFragment? EmptyTemplate { get; set; }
+    [Parameter] public string DefaultEmptyMessage { get; set; } = "List is empty.";
+
+    [Inject] IStringLocalizer<Lists.ListGroupComponent.Resources.DataListGroup> Localizer { get; set; } = default!;
 
     protected override bool GenerateUl => ItemTemplate != null;
 
@@ -43,25 +51,28 @@ public sealed class DataListGroup<TItem> : ListGroup, IDataBoundComponent<TItem>
     {
         return builder =>
         {
-            int seq = 0;
-            if (Items != null)
+            if (Items?.Any() == true)
             {
                 foreach (var item in Items)
                 {
-                    builder.OpenComponent<ListGroupItem>(seq++);
+                    builder.OpenComponent<ListGroupItem>(0);
                     if (ItemTemplate != null)
                     {
-                        builder.AddAttribute(seq++, nameof(ListGroupItem.ChildContent), ItemTemplate.Invoke(item));
+                        builder.AddAttribute(1, nameof(ListGroupItem.ChildContent), ItemTemplate.Invoke(item));
                     }
                     else
                     {
-                        builder.AddAttribute(seq++, nameof(ListGroupItem.Text), GetText(item));
-                        builder.AddAttribute(seq++, nameof(ListGroupItem.Url), GetUrl(item));
-                        builder.AddAttribute(seq++, nameof(ListGroupItem.Icon), GetIcon(item));
-                        builder.AddAttribute(seq++, nameof(ListGroupItem.Mode), GetMode(item));
+                        builder.AddAttribute(2, nameof(ListGroupItem.Text), GetText(item));
+                        builder.AddAttribute(3, nameof(ListGroupItem.Url), GetUrl(item));
+                        builder.AddAttribute(4, nameof(ListGroupItem.Icon), GetIcon(item));
+                        builder.AddAttribute(5, nameof(ListGroupItem.Mode), GetMode(item));
                     }
                     builder.CloseComponent();
                 }
+            }
+            else
+            {
+                builder.AddContent(6, GetEmptyTemplate());
             }
         };
     }
@@ -97,4 +108,33 @@ public sealed class DataListGroup<TItem> : ListGroup, IDataBoundComponent<TItem>
 
         return ListGroupItemMode.Default;
     }
+
+    private RenderFragment GetEmptyTemplate()
+    {
+        if (EmptyTemplate != null)
+            return EmptyTemplate;
+
+        return GetDefaultEmptyTemplate();
+    }
+
+    private RenderFragment GetDefaultEmptyTemplate()
+    {
+        if (_emptyTemplate == null)
+            _emptyTemplate = builder =>
+            {
+                builder.OpenElement(0, "li");
+                builder.AddAttribute(1, "class", "list-group-item text-center");
+                
+                // Icon
+                builder.OpenElement(2, "i");
+                builder.AddAttribute(3, "class", "bi bi-search me-2");
+                builder.CloseElement();
+
+                builder.AddContent(4, Localizer[DefaultEmptyMessage]);
+                builder.CloseElement();
+            };
+
+        return _emptyTemplate;
+    }
+
 }
