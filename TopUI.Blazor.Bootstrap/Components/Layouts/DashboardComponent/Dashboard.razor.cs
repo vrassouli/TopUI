d8e97a447babcfa59a3cb9630be6019d12207273
Vahid Rassouli <vrassouli@live.com>
2023-02-14 10:05:55 +0330

    DashboardLayout added

---
+++ TopUI.Blazor.Bootstrap/Components/Layouts/DashboardComponent/Dashboard.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Security.Claims;
+
+namespace TopUI.Blazor.Bootstrap.Components;
+
+public partial class Dashboard
+{
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    protected override IEnumerable<string> GetClasses()
+    {
+        foreach(var c in base.GetClasses())
+            yield return c;
+
+        yield return "tui-dashboard";
+    }
+}
