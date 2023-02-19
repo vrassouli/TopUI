a4c8fcedd19c7a0a713523bc846c7f8ae16b56fb
Vahid Rassouli <vrassouli@live.com>
2023-02-19 18:58:42 +0330

    MasterDetail component added

---
+++ TopUI.Blazor.Bootstrap/Components/Layouts/MasterDetailComponent/MasterDetail.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopUI.Blazor.Bootstrap.Components;
+
+public partial class MasterDetail
+{
+    [Parameter] public RenderFragment? Master { get; set; }
+    [Parameter] public RenderFragment? Detail { get; set; }
+    [Parameter] public Breakpoints Breakpoint { get; set; } = Breakpoints.Md;
+    [Parameter] public bool FillMaster { get; set; }
+    [Parameter] public string? MasterClasses { get; set; }
+    [Parameter] public string? DetailClasses { get; set; }
+    [Parameter] public string? DetailTitle { get; set; }
+    [Parameter] public EventCallback OnCloseDetail { get; set; }
+
+    protected override IEnumerable<string> GetClasses()
+    {
+        foreach (var c in base.GetClasses())
+            yield return c;
+        yield return "tui-master-detail";
+        yield return $"tui-master-detail-{Breakpoint}".ToLower();
+    }
+
+    protected IEnumerable<string> GetMasterClasses()
+    {
+        yield return "master";
+
+        if (FillMaster)
+            yield return "flex-fill";
+
+        if (!string.IsNullOrEmpty(MasterClasses))
+            yield return MasterClasses;
+    }
+
+    protected IEnumerable<string> GetDetailClasses()
+    {
+        yield return "detail";
+
+        if (!FillMaster)
+            yield return "flex-fill";
+
+        if (!string.IsNullOrEmpty(DetailClasses))
+            yield return DetailClasses;
+    }
+}
